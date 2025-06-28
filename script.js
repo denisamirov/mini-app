306eaed9dd75831f2d839443cf22a1b5a3081be3
denisamirov <amirowdenis@yandex.ru>
2025-06-28 16:44:53 +0300

    feat: tg name to html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 if (window.Telegram && Telegram.WebApp) {
     const user = Telegram.WebApp.initDataUnsafe.user;
-    console.log(user)
+    document.querySelector('.title').textContent = user.first_name
 }
 
 console.log('dev')
+document.querySelector('.title').textContent = 'dev'
