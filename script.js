fe9624350059610e1820f8c6b2b03165417c3e68
denisamirov <amirowdenis@yandex.ru>
2025-06-29 11:44:39 +0300

    feat: json

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
 
-  if (typeof Telegram !== 'undefined' && Telegram.WebApp) {
+//   if (typeof Telegram !== 'undefined' && Telegram.WebApp) {
 
-    Telegram.WebApp.ready();
-    
-    const user = Telegram.WebApp.initDataUnsafe?.user;
-    if (user) {
-      document.querySelector('.title').textContent = user.first_name;
-    }
-  } else {
-    console.log("Telegram WebApp не обнаружен");
-    document.querySelector('.title').textContent = 'dev';
-  }
+//     Telegram.WebApp.ready();
+
+//     const user = Telegram.WebApp.initDataUnsafe?.user;
+//     if (user) {
+//       document.querySelector('.title').textContent = user.first_name;
+//     }
+//   } else {
+//     console.log("Telegram WebApp не обнаружен");
+//     document.querySelector('.title').textContent = 'dev';
+//   }
+// });
+
+const container = document.querySelector('.card-list');
+
+ container.innerHTML = '';
+
+const productTemplate = (product) => `
+        <div class="card">
+            <img class="card-img" src="${product.image_url}" alt="картинка">
+            <div class="card-content">
+                <div class="card-content-description">
+                    <h2 class="card-price">${product.price}</h2>
+                    <p class="card-title">${product.name }</p>
+                </div>
+                <button class="buy-button">Добавить</button>
+            </div>
+        </div>
+
+`
+
+const response = await fetch('./goods.json');
+
+if (!response.ok) throw new Error('Ошибка загрузки товаров');
+
+const goods = await response.json()
+
+goods.forEach(product => {
+  container.insertAdjacentHTML('beforeend', productTemplate(product));
 });
